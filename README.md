798f6dbb2ea58c35c6b69ac8d864737ce79fa0f0
Haley Parish <heparish@ucdavis.edu>
2021-05-24 13:38:53 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align="center">README.md Generator Using Node.js</h1>
+
+## Description
+  
+This is a node.js application that uses user input from inquirer to genreate and populate a README.md file for user project. 
+  
+Below is the gif showing the functionality of the application:
+
+
+
+## Installation
+💾   
+  
+`npm init`
+  
+`npm install inquirer`
+  
+## Usage
+💻   
+  
+Run the following command at th root of your project and answer the prompted questions:
+  
+`node index.js`
--- index.js
+++ index.js
@@ -2,12 +2,82 @@
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
-const generateReadme = require("./utils/generateReadme")
-const writeFileAsync = util.promisify(fs.writeFile);
-
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "What is the title of this project?",
+        },
+        {
+            type: "input",
+            name: "motivation",
+            message: "Please write a brief description of what motivated you to create this project: "
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Describe the installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "codingStyle",
+            message: "Write what coding style you used ex.'standard'?"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Who are the contributors of this projects?"
+        },
+        {
+            type: "input",
+            name: "API",
+            message: "Were any API's used in this project?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is there a test included?"
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What do I do if I have an issue? "
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        }
+    ]);
+} 
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
